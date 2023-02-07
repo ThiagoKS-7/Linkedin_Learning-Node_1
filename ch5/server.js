0733b68ececfb0f9d6f39779f6dbfc3caee4e787
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-02-07 08:57:07 -0300

    #5-release: configurando mongodb

--- ch5/server.js
+++ ch5/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-
+const mongoose = require('mongoose');
 const app = express();
 const http = createServer(app);
 const routes = require("./routes/index.js");
@@ -12,8 +12,17 @@ require('dotenv').config();
 app.use(bodyParser.json());
 app.use("/api/v1", routes);
 io.on('connection', (socket) => {
-    console.log("user connected");
+    console.info("[SOCKET] - A new user connected!");
 })
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_STR, (err) => {
+    if (err != null) {
+        console.error("connection - ", err)
+    }
+    else {
+        console.info("[INFO] DB connection established!")
+    }
+});
 http.listen(process.env.API_PORT, () => {
-    console.log(`serving on http://localhost:${process.env.API_PORT}/api/v1`)
+    console.log(`\n- Hosting on http://localhost:${process.env.API_PORT}/api/v1\n`)
 })
